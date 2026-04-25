08c5473ad10d69073899fa48f04f9e0573276169
sofi <genuinedistinctclearcapital@gmail.com>
2026-04-25 18:03:30 -0300

    Import Service Provider

---
+++ Assets/Scripts/ServiceProvider/ServiceProvider.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IService
+{
+    public bool IsPersistent { get; }
+}
+
+public interface IDataService : IService
+{
+    public string ServiceReference { get; }
+    public object GetDataValue(string[] dataPath);
+}
+
+public sealed class ServiceProvider
+{
+    private static ServiceProvider instance;
+    public static ServiceProvider Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ServiceProvider();
+            }
+            return instance;
+        }
+        private set => instance = value;
+    }
+
+    private readonly Dictionary<string, Type> dataServiceTypeByReference = new Dictionary<string, Type>();
+    private readonly Dictionary<Type, IService> services = new Dictionary<Type, IService>();
+
+    private ServiceProvider() { }
+
+    public void AddService<ServiceType>(IService service) where ServiceType : class, IService
+    {
+        if (!services.ContainsKey(typeof(ServiceType)))
+        {
+            services.Add(typeof(ServiceType), service);
+            if (typeof(IDataService).IsAssignableFrom(typeof(ServiceType)))
+                dataServiceTypeByReference.Add((service as IDataService).ServiceReference, typeof(ServiceType));
+        }
+    }
+
+    public bool RemoveService<ServiceType>() where ServiceType : class, IService
+    {
+        if (!services.ContainsKey(typeof(ServiceType)))
+            throw new KeyNotFoundException();
+        return services.Remove(typeof(ServiceType));
+    }
+
+    public bool ContainsService<ServiceType>() where ServiceType : class, IService
+    {
+        return services.ContainsKey(typeof(ServiceType));
+    }
+
+    public ServiceType GetService<ServiceType>() where ServiceType : class, IService
+    {
+        return services[typeof(ServiceType)] as ServiceType;
+    }
+
+    public IDataService GetDataService(string serviceReference)
+    {
+        if (!dataServiceTypeByReference.ContainsKey(serviceReference))
+            return null;
+        return services[dataServiceTypeByReference[serviceReference]] as IDataService;
+    }
+
+    public void ClearAllServices()
+    {
+        services.Clear();
+        dataServiceTypeByReference.Clear();
+    }
+
+    public void ClearAllNonPersistentServices()
+    {
+        List<Type> nonPersistentServiceTypes = new List<Type>();
+        foreach (KeyValuePair<Type, IService> service in services)
+        {
+            if (!service.Value.IsPersistent)
+                nonPersistentServiceTypes.Add(service.Key);
+        }
+        foreach (Type keyToRemove in nonPersistentServiceTypes)
+        {
+            if (typeof(IDataService).IsAssignableFrom(keyToRemove))
+                dataServiceTypeByReference.Remove((services[keyToRemove] as IDataService).ServiceReference);
+            services.Remove(keyToRemove);
+        }
+    }
+}
+
